3efae56e3b53ec132c725da28dedc367c16d1e3a
zhangqian <qian.zhang@boyuanitsm.com>
2016-04-01 10:54:15 +0800

    no message

--- components/common/route.js
+++ components/common/route.js
@@ -9,6 +9,33 @@ define(['angular'], function (angular) {
             .when('/foo', {
                 templateUrl: '/templates/foo.html'
             })
+            .when('/mycenter_home', {
+                templateUrl: '/templates/mycenter/mycenter_home.html'
+            })
+             .when('/questions', {
+                templateUrl: '/templates/mycenter/questions.html'
+            })
+             .when('/settings', {
+                templateUrl: '/templates/mycenter/settings.html'
+            })
+             .when('/ideas', {
+                templateUrl: '/templates/mycenter/ideas.html'
+            })
+             .when('/system_info', {
+                templateUrl: '/templates/mycenter/system_info.html'
+            })
+             .when('/job', {
+                templateUrl: '/templates/mycenter/job.html'
+            })
+             .when('/job_2', {
+                templateUrl: '/templates/mycenter/job_2.html'
+            })
+              .when('/myfocus', {
+                templateUrl: '/templates/mycenter/myfocus.html'
+            })
+              .when('/xiugai_info', {
+                templateUrl: '/templates/mycenter/xiugai_info.html'
+            })
             // 默认路由-首页
             .otherwise({
                 redirectTo: '/home'
