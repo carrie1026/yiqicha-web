17ac829baccd9df4bbe4661bdca34c5ecf0f07da
zhangqian <qian.zhang@boyuanitsm.com>
2016-04-07 14:55:37 +0800

    搜索页面

--- components/common/route.js
+++ components/common/route.js
@@ -36,6 +36,33 @@ define(['angular'], function (angular) {
               .when('/xiugai_info', {
                 templateUrl: '/templates/mycenter/xiugai_info.html'
             })
+              .when('/nopromise_details', {
+                templateUrl: '/templates/nopromise/nopromise_details.html'
+            })
+              .when('/nopromise_business', {
+                templateUrl: '/templates/nopromise/nopromise_business.html'
+            })
+              .when('/nopromise_inquire', {
+                templateUrl: '/templates/nopromise/nopromise_inquire.html'
+            })
+               .when('/search_area', {
+                templateUrl: '/templates/search/search_area.html'
+            })
+                .when('/search_record', {
+                templateUrl: '/templates/search/search_record.html'
+            })
+                .when('/search_people', {
+                templateUrl: '/templates/search/search_people.html'
+            })
+                 .when('/search_business', {
+                templateUrl: '/templates/search/search_business.html'
+            })
+                 .when('/law_book', {
+                templateUrl: '/templates/search/law_book.html'
+            })
+                 .when('/search_nopromise', {
+                templateUrl: '/templates/search/search_nopromise.html'
+            })
             // 默认路由-首页
             .otherwise({
                 redirectTo: '/home'
