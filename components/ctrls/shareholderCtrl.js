e81c60d2477655867075a29473f57b20b9783986
yangfengyang <fy.yang@boyuanitsm.com>
2016-04-18 20:08:49 +0800

    页面内容

--- components/ctrls/shareholderCtrl.js
+++ components/ctrls/shareholderCtrl.js
@@ -1,6 +1,6 @@
 define(['./mod'], function (mod) {
     'use strict';
-mod.controller('ShareholderCtrlCtrl', ['$scope','$location', 'ShareholderService','$rootScope', function($scope, $location, ShareholderService,$rootScope) {
+mod.controller('ShareholderCtrl', ['$scope','$location', 'ShareholderService','$rootScope', function($scope, $location, ShareholderService,$rootScope) {
     // 企业信息
     console.log(123);
     ShareholderService.Shareholder($scope).then(function(data){
