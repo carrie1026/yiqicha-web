ee022ef309c5734811bbd9f3bd7bacb7376d27bf
zhangqian <qian.zhang@boyuanitsm.com>
2016-04-19 17:53:56 +0800

    主要成员

--- components/ctrls/leadpeopleCtrl.js
+++ components/ctrls/leadpeopleCtrl.js
@@ -2,9 +2,8 @@ define(['./mod'], function (mod) {
     'use strict';
 mod.controller('leadpeopleCtrl', ['$scope','$location', 'leadpeopleService','$rootScope', function($scope, $location, leadpeopleService,$rootScope) {
     // 企业信息
-    var page = 1;
-    var rows = 1;
-    leadpeopleService.leadpeople(page,rows).then(function(data){
+    var companyId
+    leadpeopleService.leadpeople(companyId).then(function(data){
         $scope.leadpeople = data;
         console.log($scope.leadpeople);
     })
