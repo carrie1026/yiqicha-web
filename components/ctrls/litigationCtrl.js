759cf28a02308e6827f876b1174d4ac082fc583f
yangfengyang <fy.yang@boyuanitsm.com>
2016-04-19 15:02:58 +0800

    页面内容

--- components/ctrls/litigationCtrl.js
+++ components/ctrls/litigationCtrl.js
@@ -2,10 +2,11 @@ define(['./mod'], function (mod) {
     'use strict';
 mod.controller('LitigationCtrl', ['$scope','$location', 'LitigationService','$rootScope', function($scope, $location, LitigationService,$rootScope) {
     // 企业信息
-    console.log(123);
-    LitigationService.Litigation($scope).then(function(data){
-        $scope.Litigation = data;
-        console.log( 321);
+    var page = 1;
+    var rows = 1;
+    LitigationService.Litigation(page,rows).then(function(data){
+        $scope.litinfod = data;
+        console.log($scope.litinfod);
     })
  }])
 });
