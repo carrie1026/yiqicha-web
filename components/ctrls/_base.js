71be6d2b4a25f91803d56d3b2dccebee3142da39
zhangqian <qian.zhang@boyuanitsm.com>
2016-04-20 17:20:07 +0800

    冲突

--- components/ctrls/_base.js
+++ components/ctrls/_base.js
@@ -16,13 +16,10 @@ define([
     'changeRecordCtrl',
     'corpinfoCtrl',
     'corpinfodeCtrl',
-<<<<<<< HEAD
     'myfocusCtrl',
     'addMyAttentionCtrl',
     'removeMyAttentionCtrl',
-=======
     'userInfoCtrl',
     'enterpriseCtrl',
-    'reviseUserCtrl',
->>>>>>> 6757d46135216c70c4c286b68e9206b7a1ac0f9d
+    'reviseUserCtrl'
 ]);
