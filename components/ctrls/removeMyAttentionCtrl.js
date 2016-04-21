99836629f0fa94e6df19121c9e608a5d932b7258
yangfengyang <fy.yang@boyuanitsm.com>
2016-04-21 17:54:25 +0800

    页面

--- components/ctrls/removeMyAttentionCtrl.js
+++ components/ctrls/removeMyAttentionCtrl.js
@@ -1,9 +1,8 @@
 define(['./mod'], function (mod) {
     'use strict';
 mod.controller('removeMyAttentionCtrl', ['$scope','$location','removeMyAttentionService','$rootScope', function($scope, $location,removeMyAttentionService,$rootScope) {
-    // 关注企业
+    // 取消关注企业
     var companyId = 12;
-    /*var accountId = 'f271b018c1ad11e5a130eca86ba4ba05';*/
     removeMyAttentionService.removeMyAttention(companyId).then(function(data){
         $scope.removeMyAttention = data;
         console.log($scope.removeMyAttention);
--- components/ctrls/shareholderCtrl.js
+++ components/ctrls/shareholderCtrl.js
@@ -1,10 +1,9 @@
 define(['./mod'], function (mod) {
     'use strict';
 mod.controller('ShareholderCtrl', ['$scope','$location', 'ShareholderService','$rootScope', function($scope, $location, ShareholderService,$rootScope) {
-    // 企业信息
+    // 股东信息
     var page = 1;
     var rows = 2;
-     console.log(1);
     ShareholderService.Shareholder(page,rows).then(function(data){
         $scope.Shareholder = data;
         console.log($scope.Shareholder);
