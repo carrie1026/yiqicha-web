ebda880d1a63751b01f3a6b1238767d1fc474486
yangfengyang <fy.yang@boyuanitsm.com>
2016-04-23 17:01:22 +0800

    页面

--- components/common/route.js
+++ components/common/route.js
@@ -4,7 +4,7 @@ define(['angular'], function (angular) {
         $routeProvider
             .when('/index', {
                 templateUrl: '/templates/index.html',
-                controller: 'indexCtrl'
+                controller: 'myfocusCtrl'
             })
             .when('/home', {
                 templateUrl: '/templates/home.html',
--- components/ctrls/searchPeopleCtrl.js
+++ components/ctrls/searchPeopleCtrl.js
@@ -6,17 +6,17 @@ mod.controller('SearchpeopleCtrl', ['$scope','$location', 'SearchpeopleService',
     var rows = 4;
     SearchpeopleService.Searchpeople(page,rows).then(function(data){
         $scope.Searchpeople = data.data;
-        console.log($scope.Searchpeople);
+//        console.log($scope.Searchpeople);
     })
     // 股东信息
     var page = 1;
     var rows = 4;
     ShareholderService.Shareholder(page,rows).then(function(data){
         $scope.Shareholder = data.data;
-        console.log($scope.Shareholder);
+//        console.log($scope.Shareholder);
     })
     // 失信信息
-    var iname = "顾海林";
+    var iname = 1234;
     dishonestyService.dishonesty(iname).then(function(data){
         $scope.dishonesty = data.data;
         console.log($scope.dishonesty);
