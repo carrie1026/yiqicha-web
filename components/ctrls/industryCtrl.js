99471ccc3b4de5738d31d33c4345a5c9f231f915
zhanghua <hua.zhang@boyuanitsm.com>
2016-04-26 12:00:41 +0800

    工商信息

--- components/ctrls/industryCtrl.js
+++ components/ctrls/industryCtrl.js
@@ -4,8 +4,7 @@ define(['./mod'], function (mod) {
     mod.controller('obtainIndustryInfoCtrl',['$scope', 'ObtainIndustryInfoService', function($scope, ObtainIndustryInfoService){
     	var fatherSectorId = '1';
     	ObtainIndustryInfoService.queryIndustryInfo(fatherSectorId).then(function(data){
-	        $scope.industryInfoArray = data.data;
-	      	console.log(data.data);
+	        $scope.industryInfo = data;
     	});
     }])
 
