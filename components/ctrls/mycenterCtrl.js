a6b5f5544b58f87f81671a2fdc6461e1eda7611b
yangfengyang <fy.yang@boyuanitsm.com>
2016-04-26 18:08:44 +0800

    纠错

--- components/ctrls/mycenterCtrl.js
+++ components/ctrls/mycenterCtrl.js
@@ -11,14 +11,15 @@ define(['./mod'], function(mod) {
             });
         };
         hotbusinessService.hotbusiness().then(function(data){
-        $scope.hotbusiness = data.data;
+            $scope.hotbusiness = data.data;
+            console.log($scope.hotbusiness);
         })
         var page = 1;
         var rows = 1;
         var accountId = 1;
         myfocusService.myfocus(page,rows,accountId).then(function(data){
             $scope.myfocus = data.data;
-            console.log($scope.myfocus);
+//            console.log($scope.myfocus);
         });  
     }])
 });
