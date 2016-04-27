c4a9beb7f7c6f6ec7a8852e0fa3547c9e0288a9f
zhanghua <hua.zhang@boyuanitsm.com>
2016-04-27 15:23:00 +0800

    bug #1480 年报列表页面，没有显示年报信息

--- components/ctrls/reportdetCtrl.js
+++ components/ctrls/reportdetCtrl.js
@@ -3,11 +3,10 @@ define(['./mod'], function (mod) {
 mod.controller('ReportdetCtrl', ['$scope','$location', 'ReportdetService','$rootScope', function($scope, $location, ReportdetService,$rootScope) {
     // 企业咨询
     var page = 1;
-    var rows = 3;
-    var companyId = 12;
+    var rows = 10;
+    var companyId = $location.search().companyId;
     ReportdetService.Reportdet(page,rows,companyId).then(function(data){
         $scope.Reportdet = data.data;
-        console.log($scope.Reportdet);
     })
  }])
 });
