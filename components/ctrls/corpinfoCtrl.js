05c9df8173fd99975b3e674c09b0c6b4e7d32d11
zhanghua <hua.zhang@boyuanitsm.com>
2016-04-27 15:35:04 +0800

    bug #1478 企业资讯页面，没有显示企业资讯内容

--- components/ctrls/corpinfoCtrl.js
+++ components/ctrls/corpinfoCtrl.js
@@ -1,13 +1,12 @@
-define(['./mod'], function (mod) {
+define(['./mod'], function(mod) {
     'use strict';
-mod.controller('CorpinfoCtrl', ['$scope','$location', 'CorpinfoService','$rootScope', function($scope, $location, CorpinfoService,$rootScope) {
-    // 企业咨询
-    var page = 1;
-    var rows = 2;
-    var companyId = 12;
-    CorpinfoService.Corpinfo(companyId,page,rows).then(function(data){
-        $scope.Corpinfo = data.data;
-        console.log($scope.Corpinfo);
-    })
- }])
+    mod.controller('CorpinfoCtrl', ['$scope', '$location', 'CorpinfoService', '$rootScope', function($scope, $location, CorpinfoService, $rootScope) {
+        // 企业咨询
+        var page = 1;
+        var rows = 10;
+        var companyId = $location.search().companyId;
+        CorpinfoService.Corpinfo(companyId, page, rows).then(function(data) {
+            $scope.Corpinfo = data.data;
+        })
+    }])
 });
