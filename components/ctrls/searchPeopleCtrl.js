37e5956570a44242f597c2f68cc4260956a71f63
zhanghua <hua.zhang@boyuanitsm.com>
2016-04-27 09:50:27 +0800

    reset qiye search

--- components/ctrls/searchPeopleCtrl.js
+++ components/ctrls/searchPeopleCtrl.js
@@ -55,6 +55,11 @@ define(['./mod'], function(mod) {
 
             // watch search box
             $scope.$watch('searchBox', function(newValue, oldValue) {
+                if ('qiye' == $scope.type && '' == newValue) {// reset qiye search 
+                    search(newValue, $scope.address);
+                    return;
+                }
+
                 if (!newValue || newValue.length < 2)
                     return;
 
