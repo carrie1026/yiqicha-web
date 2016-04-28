63027c1d1437b34209fef03fbb3216927086b918
zhanghua <hua.zhang@boyuanitsm.com>
2016-04-28 14:35:21 +0800

    Refresh index company data

--- components/ctrls/indexCtrl.js
+++ components/ctrls/indexCtrl.js
@@ -1,15 +1,39 @@
-define(['./mod'], function (mod) {
+define(['./mod'], function(mod) {
     'use strict';
-mod.controller('indexCtrl', ['$scope','$location', 'myfocusService','$rootScope', function($scope, $location,myfocusService,$rootScope) {
-    // 我关注的企业列表详情
-    /*var accountId = 'f271b018c1ad11e5a130eca86ba4ba05';*/
-    var page = 1;
-    var rows = 1;
-    var accountId = 1;
-    console.log(1);
-    myfocusService.myfocus(page,rows,accountId).then(function(data){
-        $scope.myfocus = data.data;
-        console.log($scope.myfocus);
-    });  
- }])
+
+    var COMPANY_DATA_RELOAD_TIME = 3000;
+    var MY_FORCE_PAGE = 1;
+    var MY_FORCE_ROWS = 3;
+
+    mod.controller('IndexCtrl', ['$scope', '$location', 'hotbusinessService', 'myfocusService', '$interval', '$timeout',
+        function($scope, $location, hotbusinessService, myfocusService, $interval, $timeout) {
+            hotbusinessService.hotbusiness().then(function(data) {
+                $scope.hotbusiness = data.data;
+                console.log($scope.hotbusiness);
+            })
+
+            myfocusService.myfocus(MY_FORCE_PAGE, MY_FORCE_ROWS).then(function(data) {
+                $scope.myfocus = data.data;
+            });
+
+            $scope.$watch('companyName', function(newValue, oldValue) {
+                if (!newValue || newValue.length < 2) {
+                    return;
+                }
+                $location.path('/search_people').search({
+                    type: 'qiye',
+                    companyName: newValue
+                });
+            });
+
+            // refresh company data
+            $scope.isShowCompanyData = true;
+            $interval(function() {
+                $scope.isShowCompanyData = false;
+                $timeout(function() {
+                    $scope.isShowCompanyData = true;
+                }, 10);
+            }, COMPANY_DATA_RELOAD_TIME);
+        }
+    ]);
 });
