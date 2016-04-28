ddbd2d33c48a60f7f5b450fe3e04ca0f09460d98
zhanghua <hua.zhang@boyuanitsm.com>
2016-04-28 20:21:20 +0800

    主要成员bug修复

--- components/ctrls/leadpeopleCtrl.js
+++ components/ctrls/leadpeopleCtrl.js
@@ -1,14 +1,12 @@
-define(['./mod'], function (mod) {
+define(['./mod'], function(mod) {
     'use strict';
-mod.controller('leadpeopleCtrl', ['$scope','$location', 'leadpeopleService','$rootScope', function($scope, $location,leadpeopleService,$rootScope) {
-    // 企业信息
-    var leadpeopleed = $location.search();
-    var companyId = leadpeopleed.id;
-    var page = 1;
-    var rows = 10;
-    leadpeopleService.leadpeople(companyId,page,rows).then(function(data){
-        $scope.leadpeople = data.data;
-        console.log($scope.leadpeople);
-    })
- }])
+    mod.controller('leadpeopleCtrl', ['$scope', '$location', 'leadpeopleService', '$rootScope', function($scope, $location, leadpeopleService, $rootScope) {
+        // 企业信息
+        var leadpeopleed = $location.search();
+        var companyId = leadpeopleed.companyId;
+        leadpeopleService.leadpeople(companyId, -1, -1).then(function(data) {
+            $scope.leadpeople = data.data;
+            console.log($scope.leadpeople);
+        })
+    }])
 });
