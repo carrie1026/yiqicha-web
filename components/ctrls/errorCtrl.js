9bad394772d10eeae0e96e5a219e03f3c98b1b28
zhanghua <hua.zhang@boyuanitsm.com>
2016-04-29 10:39:22 +0800

    修复纠错信息

--- components/ctrls/errorCtrl.js
+++ components/ctrls/errorCtrl.js
@@ -1,35 +1,35 @@
-define(['./mod'], function (mod) {
+define(['./mod'], function(mod) {
     'use strict';
-    mod.controller('ErrorCtrl', ['$scope', '$location', 'ErrorService','$rootScope', function($scope, $location, ErrorService,$rootScope) {
+    mod.controller('ErrorCtrl', ['$scope', '$location', 'ErrorService', '$rootScope', function($scope, $location, ErrorService, $rootScope) {
         // init registerFormData
-            $scope.ErrorFormData = {}; 
-//            $scope.selected = {};
-            var key = 'er_defect_management';
-            ErrorService.Errortype(key).then(function(data){
-                $scope.Errortype = data.data;
-                console.log($scope.Errortype);
-                $scope.ErrorFormData.errorParts = $scope.selected;
-            })
-          
-            $scope.subError = function(dictName){ 
-                $scope.ErrorFormData.errorParts = dictName;
-                console.log( $scope.ErrorFormData.errorParts);
-                
-            }
-            $scope.submitBtn = function() {
-                console.log( $scope.ErrorFormData.errorParts);
-                console.log($scope.ErrorFormData);
-                var promise = ErrorService.Error($scope.ErrorFormData);
-                promise.then(function(data) {
-                    layer.open({
-                    content: '提交成功',
+        $scope.ErrorFormData = {};
+        //            $scope.selected = {};
+        var key = 'er_defect_management';
+        ErrorService.Errortype(key).then(function(data) {
+            $scope.Errortype = data.data;
+            console.log($scope.Errortype);
+            $scope.ErrorFormData.errorParts = $scope.selected;
+        })
+
+        $scope.subError = function(dictName) {
+            $scope.ErrorFormData.errorParts = dictName;
+            console.log($scope.ErrorFormData.errorParts);
+
+        }
+        $scope.submitBtn = function() {
+            console.log($scope.ErrorFormData.errorParts);
+            console.log($scope.ErrorFormData);
+            var promise = ErrorService.Error($scope.ErrorFormData);
+            promise.then(function(data) {
+                layer.open({
+                    content: '提交成功, 感谢您的反馈',
                     style: 'background-color:#fff; color:#999; border:none;',
                     time: 2
                 });
-                    history.back(-1);
-                }, function(data) {
-                     alert('系统异常');
-                });
-            };
-      }])
+                history.back(-1);
+            }, function(data) {
+                alert('系统异常');
+            });
+        };
+    }])
 });
